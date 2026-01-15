1ab137589cea7b21c3e2b3231f3f8f349df9e688
ramil-bitrise <ramil.ianbulatov@bitrise.io>
2026-01-15 16:10:39 +0200

    install go-githubactions dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bitrise-io/github-cache
 
 go 1.24.1
+
+require github.com/sethvargo/go-githubactions v1.3.2
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  "github.com/sethvargo/go-githubactions"
+)
+
+func main() {
+  val := githubactions.GetInput("val")
+  if val == "" {
+    githubactions.Fatalf("missing 'val'")
+  }
+}
+
