a75e0d2ff4065fb50c080041f8582b4b7402d6cd
ramil-bitrise <ramil.ianbulatov@bitrise.io>
2026-01-16 13:06:58 +0200

    touch .envstore.yml (needed for envman)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+# Create .envstore.yml if it doesn't exist (required by envman)
+if [ ! -f "${GITHUB_WORKSPACE}/.envstore.yml" ]; then
+    touch "${GITHUB_WORKSPACE}/.envstore.yml"
+fi
+
+# Set ENVMAN_ENVSTORE_PATH so envman knows where to find the envstore
+export ENVMAN_ENVSTORE_PATH="${GITHUB_WORKSPACE}/.envstore.yml"
+
 # Run the bitrise-cache binary
 # Phase detection (restore vs save) is handled internally via state variables
 exec /bitrise-cache
